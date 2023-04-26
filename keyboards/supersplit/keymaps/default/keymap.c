01b2d2aa47fd1fd9316a5fa892bacb4b4629f4cc
tarneo <tarneo@tarneo.fr>
2023-04-26 12:22:04 +0200

    Diable auto shift and new key at shift position
    This reverts commit e3e1ccb2513267b65b18275ede192c7d84ed8b33.

--- keyboards/supersplit/keymaps/default/keymap.c
+++ keyboards/supersplit/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_NO, KC_A, KC_R, KC_S, KC_T, KC_D,
                              KC_NO, KC_Z, KC_X, KC_C, KC_G, KC_V,
                                                                          KC_NO, KC_NO, KC_NO,
-                             KC_LALT, MT(MOD_LGUI,KC_TAB), LT(1,KC_SPC), KC_LSFT, KC_NO, KC_NO,
+                             KC_LGUI, MT(MOD_LSFT,KC_TAB), LT(1,KC_SPC), KC_LALT, KC_NO, KC_NO,
 
                                                                                                                               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                                                                                                                               KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_PSCR,
