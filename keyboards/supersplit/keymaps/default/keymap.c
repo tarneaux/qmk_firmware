7caf03a17b5bdb32c65ec9da67ad1edbc7ac7001
tarneo <tarneo@renn.es>
2023-05-24 17:06:07 +0200

    Add function keys on upper row of base layer

--- keyboards/supersplit/keymaps/default/keymap.c
+++ keyboards/supersplit/keymaps/default/keymap.c
@@ -13,14 +13,14 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_6x4_9(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+	[0] = LAYOUT_split_6x4_9(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
                              KC_F18, KC_Q, KC_W, KC_F, KC_P, KC_B,
                              KC_NO, KC_A, KC_R, KC_S, KC_T, KC_D,
                              KC_NO, KC_Z, KC_X, KC_C, KC_G, KC_V,
                                                                          KC_NO, KC_NO, KC_NO,
                              KC_LGUI, MT(MOD_LSFT,KC_TAB), LT(1,KC_SPC), KC_LALT, KC_NO, KC_NO,
 
-                                                                                                                              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                                                                                                              KC_F7, KC_F8, KC_F9, KC_F10, KC_F12, KC_NO,
                                                                                                                               KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_PSCR,
                                                                                                                               KC_H, KC_N, KC_E, KC_I, KC_O, KC_ENT,
                                                                                                                               KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
